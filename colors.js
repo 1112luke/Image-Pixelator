b94ab13500b9553c123faa69c3849f3f6260736e
Lynley Pace <lynley@dhcp-10-7-81-80.dhcp.nd.edu>
2023-11-16 13:03:57 -0500

    color attempt 1

---
+++ colors.js
@@ -0,0 +1,20 @@
+class Color {
+    constructor(id, hex){
+        this.id = id;
+        this.color = hex; 
+    
+    }
+    convertToRGB(){
+        this.color = this.color.replace("#","");
+        var bigint = parseInt(this.color, 16);
+        var r = (bigint >> 16)& 255;
+        var g = (bigint >> 8) & 255;
+        var b = bigint & 255;
+        this.color = [r, g, b, 255];
+
+    }
+}
+var prismacolors = [
+    Color("Apple Green", "#4DAD44")
+]
+console.log(prismacolors[0].convertToRGB());
