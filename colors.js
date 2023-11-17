95935398d2274ed00679f6ecd8029e0cd7689f13
Lynley Pace <lynley@lynleys-mbp.dhcp.nd.edu>
2023-11-16 23:39:49 -0500

    colors added

--- colors.js
+++ colors.js
@@ -4,8 +4,8 @@ class Color {
         this.hex = hex;
 
         //hex value to rgba value
-        convertToRGB(){
-            var newhex = ; 
+        convertToRGB()
+            var newhex 
             this.newhex = this.hex.replace("#","");
             var bigint = parseInt(this.newhex, 16);
             var r = (bigint >> 16)& 255;
@@ -13,9 +13,27 @@ class Color {
             var b = bigint & 255;
             this.rgba = [r, g, b, 255];
     }
-    }
 }
+
 var prismacolors = [
+    Color("10% Cool Grey", "E6E8E8"), 
+    Color("10% French Grey", "E9E7DD"),
+    Color("10% Warm Grey", "EAE8EA"),
+    Color("20% Cool Grey", "D9DDE2"), 
+    Color("20% French Grey", "D4D3C9"),
+    Color("20% Warm Grey", "D5D3D5"),
+    Color("30% Cool Grey", "C7CED5"), 
+    Color("30% French Grey", "C1BFB8"),
+    Color("30% Warm Grey", "C2BBBB"),
+    Color("50% Cool Grey", "A7B0B8"), 
+    Color("50% French Grey", "ABA4A1"),
+    Color("50% Warm Grey", "A299A1"),
+    Color("70% Cool Grey", "7C8C8E"), 
+    Color("70% French Grey", "7B6D62"),
+    Color("70% Warm Grey", "6D6262"),
+    Color("90% Cool Grey", "2D4A5A"), 
+    Color("90% French Grey", "292726"),
+    Color("90% Warm Grey", "554A4A"),
     Color("Apple Green", "#4DAD44"),
     Color("Aquamarine", "#008C9E"), 
     Color("Artichoke", "#7F7315"), 
@@ -92,19 +110,61 @@ var prismacolors = [
     Color("Mediterranean Blue", "#007E9E"), 
     Color("Metallic Gold", "#AC8B3C"), 
     Color("Mineral Orange", "#E68C3F"),
-
-
-
+    Color("Moss Green", "#6E6D13"), 
+    Color("Mulberry", "#A9007B"), 
+    Color("Muted Turquoise", "#51B7C3"), 
+    Color("Nector", "#E2947B"),
+    Color("Neon Orange", "#FF7716"), 
+    Color("Neon Pink", "#FF34C0"), 
+    Color("Neon Yellow", "#FFFF3B"), 
+    Color("Non-Photo Blue", "#00BBD4"), 
+    Color("Olive Green", "#3E5939"), 
+    Color("Orange", "#FF6E12"), 
+    Color("Pale Sage", "#DDEAC6"), 
+    Color("Pale Vermilion", "#FA6610"), 
+    Color("Parma Violet", "#8E69BA"),
+    Color("Parrot Green", "#00B092"), 
+    Color("Peach", "#F1A78D"),
+    Color("Peach Beige", "#F8E4D6"), 
+    Color("Peacock Blue", "#00739A"), 
+    Color("Peacock Green", "#004729"), 
+    Color("Permanent Red", "#F60000"), 
+    Color("Periwinkle", "#50549B"), 
+    Color("Pink", "#F3889E"), 
+    Color("Pink Rose", "#ECB7C1"), 
+    Color("Pomegranate", "#870038"),
+    Color("Poppy Red", "#FF4908"), 
+    Color("Powder Blue", "C3D8EE"), 
+    Color("Process Red", "#D74893"), 
+    Color("Pumpkin Orange", "#DE6619"), 
+    Color("Putty Beige", "#DECDC7"), 
+    Color("Raspberry", "#A31719"), 
+    Color("Rosy Beige", "#D1BDC1"), 
+    Color("Prussian Green", "#113B15"), 
+    Color("Salmon Pink", "#F9AF9C"), 
+    Color("Sand", "#FAE0B7"), 
+    Color("Sandbar Brown", "#866210"), 
+    Color("Sap Green Light", "#78A87C"), 
+    Color("Scarlet Lake", "#D22909"), 
+    Color("Seashell Pink", "#F4D1C4"),
+    Color("Sepia", "#352D14"),
+    Color("Sienna Brown", "#9F430A"), 
+    Color("Silver", "#A3A1A3"), 
+    Color("Sky Blue Light", "#B1E1F0"), 
+    Color("Slate Grey", "#347E8F"), 
+    Color("Spanish Orange", "#FFC12A"), 
+    Color("Spring Green", "#85CF66"), 
+    Color("Sunburst Yellow", "#FFD62F"), 
+    Color("Terra Cotta", "#A14107"), 
+    Color("True Blue", "#007EB4"), 
+    Color("True Green", "#0BBF7E"), 
+    Color("Tuscan", "#6F2436"), 
+    Color("Ultramarine", "#19189E"), 
+    Color("Violet", "#44308F"), 
+    Color("Violet Blue", "#000092"), 
+    Color("White", "#FFFFFF"), 
+    Color("Yellow Chartreuse", "#FFF991"), 
+    Color("Yellow Ochre", "#DAAD41"), 
+    Color("Yellowed Orange", "#FFA723")
 ]
 console.log(prismacolors[0].convertToRGB());
-
-for (var m = 0; m < )
-for(var i = 0; i < pixels.length; i++){
-    var dist; 
-    var prismaX = pixels[i][0];
-    var prismaY = pixels[i][1];
-    var prismaZ = pixels[i][2];
-    var p
-    dist = Math.sqrt(Math.pow(prismaX -  ) + Math.pow(prismaY - ) + Math.pow(prismaZ - ))
-
-}
