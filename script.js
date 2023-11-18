5cd5068840ad5610413a693e8bb3b55802310a6a
1112luke <128310124+1112luke@users.noreply.github.com>
2023-11-18 12:56:50 -0500

    fixed class

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ class Color {
         this.id = id;
         this.hex = hex;
         this.selected = true; 
+    }
 
-        //hex value to rgba value
-        convertToRGB()
-            var newhex 
-            this.newhex = this.hex.replace("#","");
-            var bigint = parseInt(this.newhex, 16);
-            var r = (bigint >> 16)& 255;
-            var g = (bigint >> 8) & 255;
-            var b = bigint & 255;
-            this.rgba = [r, g, b, 255];
+    //hex value to rgba value
+    convertToRGB(){
+        var newhex 
+    this.newhex = this.hex.replace("#","");
+    var bigint = parseInt(this.newhex, 16);
+    var r = (bigint >> 16)& 255;
+    var g = (bigint >> 8) & 255;
+    var b = bigint & 255;
+    this.rgba = [r, g, b, 255];
     }
 }
 
