7759779a7550158b0043a04bdee293baa3399535
1112luke <128310124+1112luke@users.noreply.github.com>
2023-11-20 09:00:10 -0500

    added cors override

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const cors = require("cors");
+
+app.use(cors());
+
 const fs = require("fs");
 
 app.get('/:fileName', (req, res) => {
